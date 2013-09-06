b5aa39a61f8ddc696fbecdd8f4629b9685e6893c
Alex Houchens <gregoryx.houchens@intel.com>
2013-09-06 19:15:37 +0000

    EPSD100236143 fixed. MLE page getting into a strange state is now addressed.

--- scripts/controllers/trustMles-controller.js
+++ scripts/controllers/trustMles-controller.js
@@ -12,20 +12,10 @@ App.TrustMlesController = Ember.ArrayController.extend(App.Filterable, App.Sorta
         App.event('Failed to delete fingerprint, you must first remove trust from all of the fingerprint\'s associated nodes.', App.ERROR);
       } else {
         var confirmed = confirm('Are you sure you want to delete this fingerprint?');
-        if (confirmed) {          
-          var ajaxOptions = $.extend({
-            type: 'DELETE',
-            url: ((!localStorage.apiDomain) ? '' : '//' + localStorage.apiDomain) + '/api/v1/trust_mles/' + mle.get('id'),
-            contentType: "application/json",
-            dataType: "json"
-          }, App.ajaxSetup);
-          App.ajaxPromise(ajaxOptions).then(function (data, textStatus, jqXHR) {
-            mle.deleteRecord();
-            App.event('Successfully deleted fingerprint "' + mle.get('name') + '"', App.SUCCESS);
-          }, function (qXHR, textStatus, errorThrown) {
-            App.event('Failed to delete fingerprint "' + mle.get('name') + '"', App.ERROR);
-          });
-          //mle.get('transaction').commit();
+        if (confirmed) {
+          mle.deleteRecord();
+          mle.get('transaction').commit();
+          mle.get('store').commit();
         }
       }
     });
@@ -38,11 +28,4 @@ App.TrustMlesController = Ember.ArrayController.extend(App.Filterable, App.Sorta
       this.transitionToRoute('trust.mles');
     }
   }
-  
-  /*
-  frameUrl: function () {
-    // Mt. Wilson Whitelist Portal
-    return 'https://' + window.location.hostname + ':8181/WhiteListPortal/';
-  }.property()
-  */
 });
