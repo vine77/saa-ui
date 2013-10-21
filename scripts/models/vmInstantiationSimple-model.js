40c30a53616884322bcc19268579b4adb782c1ff
Alex Houchens <gregoryx.houchens@intel.com>
2013-10-21 02:57:27 +0000

    Instantiation simple and detailed refactoring with latest ember, including new per model serializers for munging the json into sideloaded records. This commit also includes new models for the embedded data.

--- scripts/models/vmInstantiationSimple-model.js
+++ scripts/models/vmInstantiationSimple-model.js
@@ -1,3 +1,44 @@
+App.VmInstantiationSimpleSerializer = DS.ActiveModelSerializer.extend({
+  extractSingle: function(store, primaryType, payload, recordId, requestType) {
+    var payload = JSON.parse(JSON.stringify(payload)),
+        slo_gates = payload.vm_instantiation_simple.slo_gates,
+        slo_gate_ids = slo_gates.mapProperty('slo_id'),
+        ranked_nodes = payload.vm_instantiation_simple.ranked_nodes,
+        ranked_node_ids = ranked_nodes.mapProperty('node_id');
+
+    payload.slo_gates = slo_gates;
+    payload.vm_instantiation_simple.slo_gate_ids = slo_gate_ids;
+
+    payload.slo_gates.map ( function (item, index, enumerable) {
+      item.id = item.slo_id;
+      item.vm_instantiation_simple_id = this.toString();
+    }, payload.vm_instantiation_simple.id);
+
+    payload.ranked_nodes = ranked_nodes;
+    payload.vm_instantiation_simple.ranked_node_ids = ranked_node_ids;
+
+    payload.ranked_nodes.map ( function (item, index, enumerable) {
+      item.id = item.node_id;
+      item.vm_instantiation_simple_id = this.id.toString();
+    }, payload.vm_instantiation_simple);
+
+    return this._super(store, primaryType, payload, recordId, requestType);
+  }
+}); 
+
+App.SloGate = DS.Model.extend({
+  slo: DS.belongsTo('slo'),
+  vmInstantiationSimple: DS.belongsTo('vmInstantiationSimple'),
+  description: DS.attr('string'),
+  nodesCount: DS.attr('string')
+});
+
+App.RankedNode = DS.Model.extend({
+  node: DS.belongsTo('node'),
+  selected: DS.attr('boolean'),
+  vmInstantiationSimple: DS.belongsTo('vmInstantiationSimple')
+});
+
 App.VmInstantiationSimple = DS.Model.extend({
   generationTime: DS.attr('date'),
   instantiationTrusted: function () {
@@ -10,10 +51,10 @@ App.VmInstantiationSimple = DS.Model.extend({
     return trustFound;
   }.property('sloGates.@each'),
   nodesCount: DS.attr(),
-  rankedNodes: DS.attr(),
+  rankedNodes: DS.hasMany('rankedNode'),
   scheduleTime: DS.attr('string'),
   slaName: DS.attr('string'),
-  sloGates: DS.attr(),
+  sloGates: DS.hasMany('sloGate'),
   vmName: DS.attr('string'),
   vmTrustStatus: DS.attr('boolean'),
 
