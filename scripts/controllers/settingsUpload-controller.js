f478b9a4e4903181ffdd5bcbdff5c3716430012a
Nathan Ward <ward@digivine.com>
2013-10-24 06:11:00 -0700

    Add client-side validation to require all three config files before uploading

--- scripts/controllers/settingsUpload-controller.js
+++ scripts/controllers/settingsUpload-controller.js
@@ -9,6 +9,12 @@ App.SettingsUploadController = Ember.ArrayController.extend({
   quantumSuccessBinding: 'App.quantum.success',
   actions: {
     uploadFiles: function () {
+      // Require all 3 files to be specified
+      var allFilesSpecified = !!$('#novaForm').find('input[type=file]').val() && !!$('#openrcForm').find('input[type=file]').val() && !!$('#quantumForm').find('input[type=file]').val();
+      if (!allFilesSpecified) {
+        App.event('You must upload all three configuration files at the same time.');
+        return;
+      }
       // Prompt user with confirmation dialog if app is already configured
       var confirmUpload = true;
       if (this.get('isEnabled')) confirmUpload = confirm('Are you sure you want to upload new configuration files and restart ' + App.application.get('title') + '?');
