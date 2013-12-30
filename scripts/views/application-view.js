8b5859ff1ffa89b28f525edaba1be233bfc65fce
Alex Houchens <gregoryx.houchens@intel.com>
2013-12-30 04:16:38 +0000

    CQ EPSD100236248: Now implements a fixed table of contents.

--- scripts/views/application-view.js
+++ scripts/views/application-view.js
@@ -1,4 +1,15 @@
 App.ApplicationView = Ember.View.extend({
   tagName: 'div',
-  classNames: ['application']
+  classNames: ['application'],
+  styleSidebar: function() {
+    var docsIframe = $('#docs');
+    docsIframe.contents().find('.sphinxsidebarwrapper').css({"overflow-y":"scroll", "overflow-x":"hidden", "position":"fixed", "width":"222px", "height":"770px"});
+  },
+  didInsertElement: function() {
+    var self = this;
+    $('#docs').load(function(){
+      self.styleSidebar();
+    });
+    Ember.run.later(this, 'styleSidebar', 5000);
+  }
 });
