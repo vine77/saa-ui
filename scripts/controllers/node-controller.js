33d193f2698de6fc4d0385697b3940eb3b8776e4
Alex Houchens <gregoryx.houchens@intel.com>
2014-01-13 18:19:35 +0000

    EPSD100237160: Restructured table data for services in node details.
    Conflicts:
    	overlays/ipm/rootfs/srv/www/source/scripts/templates/nodesNode.handlebars

--- scripts/controllers/node-controller.js
+++ scripts/controllers/node-controller.js
@@ -420,3 +420,28 @@ App.NodeController = Ember.ObjectController.extend({
 });
 
 App.NodesNodeController = App.NodeController.extend();
+
+App.ServiceController = Ember.ObjectController.extend({
+  nodeType: function () {
+    var name = this.get('name');
+    if (name.length < 1) {
+      return 'generic';
+    } else {
+      return name;
+    }
+    return 'generic';
+  }.property('name'),
+  nodeTypeClass: function () {
+    return 'icon-' + this.get('nodeType');
+  }.property('nodeType'),
+  overallHealth: function() {
+     return App.overallHealth(health, operational);
+  }.property(),
+  healthMessage: function() {
+    return 'Healthy State: ' + App.priorityToType(this.get('health')).capitalize();
+  }.property('health'),  
+  operationalMessage: function() {
+    return 'Operational State: ' + App.codeToOperational(this.get('operational')).capitalize();
+  }.property('operational'),
+
+});
