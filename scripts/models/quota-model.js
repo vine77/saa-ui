dec5493801d61d6434002217a5e290530115dba6
Alex Houchens <gregoryx.houchens@intel.com>
2014-01-14 21:26:03 +0000

    EPSD100239144 & EPSD100239068: API endpoint has been updated to use api/v1/configuration for quotas.json. Current RAM usage message updated.

--- scripts/models/quota-model.js
+++ scripts/models/quota-model.js
@@ -1,5 +1,7 @@
 Ember.Inflector.inflector.irregular('quota', 'quotas');
 
+App.QuotaAdapter = DS.RESTConfigAdapter.extend();
+
 App.Quota = DS.Model.extend({
   injectedFileContentBytes: DS.attr('number'),
   metadataItems: DS.attr('number'),
