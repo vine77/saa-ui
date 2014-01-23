e38621c585e8e39c88f4a4c294a50e015c585987
Alex Houchens <gregoryx.houchens@intel.com>
2014-01-23 20:57:47 +0000

    WIP changes

--- scripts/controllers/slasCreate-controller.js
+++ scripts/controllers/slasCreate-controller.js
@@ -27,3 +27,58 @@ App.SlasCreateController = Ember.ObjectController.extend({
     }
   }
 });
+
+App.SloCreateController = Ember.ObjectController.extend({
+  sloTemplates: function () {
+    return this.store.all('sloTemplate');
+  }.property(),
+
+  isBooleanSelected: 0,
+  integer: '',
+  rangeMin: '',
+  rangeMax: '',
+
+  valueModify: function (key, value) {
+    console.log('value is executed');
+
+    if (arguments.length > 1) {
+      console.log('setter is executed');
+    }
+    if (value) {
+      console.log('setter is executed');
+    }
+
+    if (this.get('model.isRange')) {
+      //var isRangeParts = value.split(/\s+/);
+      //this.set('firstName', nameParts[0]);
+      //this.set('lastName',  nameParts[1]);
+
+      var rangeMin = (( this.get('rangeMin') )?this.get('rangeMin'):'');
+      var rangeMax = (( this.get('rangeMax') )?this.get('rangeMax'):'');
+      return rangeMin + ';' + rangeMax;
+    }
+    if (this.get('model.isBoolean')) {
+      return this.get('isBooleanSelected');
+    }
+    if (this.get('model.isInteger')) {
+      return this.get('integer');
+    }
+    return '';
+  }.property('rangeMin', 'rangeMax', 'isBooleanSelected', 'integer', 'sloTemplate', 'model.value'),
+  init: function() {
+    this.get('value');
+  }
+
+});
+
+Ember.RadioButton = Ember.View.extend({
+    tagName : "input",
+    type : "radio",
+    attributeBindings : [ "name", "type", "value", "checked:checked:" ],
+    click : function() {
+        this.set("selection", this.$().val())
+    },
+    checked : function() {
+        return this.get("value") == this.get("selection"); 
+    }.property()
+});
