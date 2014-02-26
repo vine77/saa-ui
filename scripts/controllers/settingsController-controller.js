34a8bd5fc16052eb3be6de7dc50531b18334a0ca
Alex Houchens <gregoryx.houchens@intel.com>
2014-02-26 23:55:27 +0000

    EPSD100238045: Added additional warning language for both actions reset and shutdown of controller.

--- scripts/controllers/settingsController-controller.js
+++ scripts/controllers/settingsController-controller.js
@@ -4,7 +4,7 @@ App.SettingsControllerController = Ember.Controller.extend({
     reset: function () {
       this.set('isActionPending', true);
       var self = this;
-      var confirmed = confirm('Are you sure you want to reset the controller?');
+      var confirmed = confirm('Are you sure you want to reset the controller? Warning: Resetting the controller may result in an unavailable UI for a duration of time, and depending on your network configuration the UI IP may change.');
       if (confirmed) {
         this.store.createRecord('action', {
           name: 'reset_controller'
@@ -22,16 +22,16 @@ App.SettingsControllerController = Ember.Controller.extend({
     shutdown: function() {
       this.set('isActionPending', true);
       var self = this;
-      var confirmed = confirm('Are you sure you want to shutdown the controller?');
+      var confirmed = confirm('Are you sure you want to shutdown the controller? Warning: Shutting down the controller may result in an unavailable UI.');
       if (confirmed) {
         this.store.createRecord('action', {
-          name: 'reset_controller'
+          name: 'shutdown_controller'
         }).save().then(function () {
           self.set('isActionPending', false);
-          App.event('Successfully reset controller', App.SUCCESS);
+          App.event('Successfully shutdown controller', App.SUCCESS);
         }, function (xhr) {
           self.set('isActionPending', false);
-          App.xhrError(xhr, 'Failed to reset controller');
+          App.xhrError(xhr, 'Failed to shutdown controller');
         });
       } else {
         self.set('isActionPending', false);
