2bab66ab1e0d1a7b404ac6d5cbe27e174826e7bf
Nathan Ward <nathanx.j.ward@intel.com>
2014-03-31 12:08:04 -0700

    Update product name to prefix Service Assurance Administrator or Service Assurance Administrator Solution with Intel® Datacenter Manager (and remove references to outdated name, Cloud Orchestrator
    CQ: EPSD100240155

--- scripts/models/application-model.js
+++ scripts/models/application-model.js
@@ -1,9 +1,9 @@
 App.Application = Ember.Object.extend({
-  fullTitle: 'Intel® Service Assurance Administrator',
+  fullTitle: 'Intel® Datacenter Manager: Service Assurance Administrator',
   longTitle: 'Service Assurance Administrator',
   title: 'SAA',
   year: function () {
-    return moment().format('YYYY')
+    return moment().format('YYYY');
   }.property(),
   apiDomain: function () {
     return localStorage.apiDomain;
