96e2003ed0f3f7fc4348f1f2acb06625c3c98cdc
Alex Houchens <gregoryx.houchens@intel.com>
2014-04-07 16:15:46 +0000

    EPSD100240783: Final addressing of kibana3 performance, by removal logBar dynamic filters and initialization filters.

--- scripts/views/logBar-view.js
+++ scripts/views/logBar-view.js
@@ -1,12 +1,3 @@
 App.LogBarView = Ember.View.extend({
-  templateName: 'logBar-view',
-  timeFieldId: null,
-  didInsertElement: function() {
-    var self = this;
-    setTimeout( function() {
-      Ember.run(function(){
-        self.get('controller').set('criticalitySelected', self.get('controller.controllers.criticalities').objectAt(4));
-      });
-    }, 10000);
-  }
+  templateName: 'logBar-view'
 });
