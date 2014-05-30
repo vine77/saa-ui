abdd4324a6355ff1b47e2ddcaccbc887c274cf89
Nathan Ward <nathanx.j.ward@intel.com>
2014-05-30 09:37:34 -0700

    Update format of HA/scheduler status messaging on SAA dashboard

--- scripts/controllers/dashboardStatus-controller.js
+++ scripts/controllers/dashboardStatus-controller.js
@@ -2,34 +2,22 @@ App.DashboardStatusController = Ember.ArrayController.extend({
   needs: ['application', 'statuses', 'build', 'nodes', 'vms'],
   haMessages: function() {
     var messages = [];
-    statuses = this.get('controllers.statuses.model');
-    cloudScheduling = statuses.filterBy('id', 'cloud_scheduling_service');
-    cloudSchedulingMessage = cloudScheduling.get('message');
-    schedulerPluginVersion = statuses.filterBy('id', 'scheduler_plugin_version');
-    schedulerPluginVersionMessage = schedulerPluginVersion.get('message');
-
-    if (statuses) {
-      if (!Ember.isEmpty(cloudScheduling)) {
-        if (!Ember.isEmpty(cloudSchedulingMessage)) {
-          messages.addObject({ styleClass: 'text-success', message: cloudSchedulingMessage}); 
-        } else {
-          messages.addObject({ styleClass: 'text-warning', message: 'Cloud scheduling status message is empty.'});
-        }
-      } else {
-        messages.addObject({ styleClass: 'text-warning', message: 'Cloud scheduling status is not available.'});
-      }
-      if (!Ember.isEmpty(schedulerPluginVersion)) {
-        if (!Ember.isEmpty(schedulerPluginVersionMessage)) {
-          messages.addObject({ styleClass: 'text-success', message: schedulerPluginVersionMessage}); 
-        } else {
-          messages.addObject({ styleClass: 'text-warning', message: 'Scheduler plugin version status message is empty.'});
-        }
-      } else {
-        messages.addObject({ styleClass: 'text-warning', message: 'Scheduler plugin version status is not available.'});
-      }
-    } else {
-      messages.addObject({ styleClass: 'text-warning', message: 'System statuses are not currently available. Please check configuration.'});
-    }
+    cloudSchedulingService = this.get('controllers.statuses.model').findBy('id', 'cloud_scheduling_service');
+    cloudSchedulingServiceMessage = cloudSchedulingService && this.get('controllers.statuses.model').findBy('id', 'cloud_scheduling_service').get('message');
+    schedulerPluginVersion = this.get('controllers.statuses.model').findBy('id', 'scheduler_plugin_version');
+    schedulerPluginVersionMessage = cloudSchedulingService && this.get('controllers.statuses.model').findBy('id', 'scheduler_plugin_version').get('message');
+    if (!schedulerPluginVersionMessage && !schedulerPluginVersionMessage) return [{
+      title: 'High Availability',
+      message: 'N/A'
+    }];
+    if (cloudSchedulingServiceMessage) messages.push({
+      title: 'Cloud Scheduling Services',
+      message: cloudSchedulingServiceMessage
+    });
+    if (schedulerPluginVersionMessage) messages.push({
+      title: 'Scheduler Plugins',
+      message: schedulerPluginVersionMessage
+    });
     return messages;
   }.property('controllers.statuses.@each')
 });
