58dcb67bc7f4c749b2c4b2e6214966713c71f5ec
Nathan Ward <nathanx.j.ward@intel.com>
2014-07-08 15:12:48 -0700

    Refactor stacked progress bar component controller to simply width calculation and fix bug in last segment width

--- scripts/controllers/progressBarStackedComponent-controller.js
+++ scripts/controllers/progressBarStackedComponent-controller.js
@@ -3,36 +3,30 @@ App.ProgressBarStackedComponent = Ember.Component.extend({
   thresholdOne: null,
   thresholdTwo: null,
   thresholdThree: null,
-  thresholdMax: function() {
-    return this.get('thresholdThree');
-  }.property('thresholdThree'),
-  areEvenThresholds: function() {
-    return ((this.get('thresholdOne') - 0) == (this.get('thresholdTwo') - this.get('thresholdOne')) == (this.get('thresholdThree') - this.get('thresholdTwo')));
-  }.property('thresholdOne', 'thresholdTwo', 'thresholdThree'),
+  thresholdMax: Ember.computed.alias('thresholdThree'),
   backgroundWidths: function() {
     var sum = Number(this.get('progressBarTwoPercent')) + Number(this.get('progressBarOnePercent'));
-    var returnValue = 'background-image:'+
-    'linear-gradient('+
-    ' to right,'+
-    ' rgba(94, 185, 94, 0.3), '+
-    ' rgba(94, 185, 94, 0.3) ' + this.get('progressBarOnePercent') + '%,'+
-    ' rgba(250, 167, 50, 0.3) '+ this.get('progressBarOnePercent') + '%,'+
-    ' rgba(250, 167, 50, 0.3) '+ sum + '%,' +
-    ' rgba(221, 81, 76, 0.3) ' + sum + '%' +
-    ');';
-    return returnValue;
+    return 'background-image:'+
+      'linear-gradient('+
+      ' to right,'+
+      ' rgba(94, 185, 94, 0.3), '+
+      ' rgba(94, 185, 94, 0.3) ' + this.get('progressBarOnePercent') + '%,'+
+      ' rgba(250, 167, 50, 0.3) '+ this.get('progressBarOnePercent') + '%,'+
+      ' rgba(250, 167, 50, 0.3) '+ sum + '%,' +
+      ' rgba(221, 81, 76, 0.3) ' + sum + '%' +
+      ');';
   }.property('progressBarOnePercent', 'progressBarTwoPercent', 'progressBarThreePercent'),
-  styles: function() {
-    return 'width:' + this.get('overallWidth') + '; ' + 
-    this.get('backgroundWidths') +
-    'overflow:visible;'+
-    'position:relative;'+
-    'height:10px;'+
-    'margin-top:10px;'+
-    'margin-left:5px;'+
-    'text-align:center;';
+  progressStyle: function() {
+    return 'width:' + this.get('overallWidth') + '; ' +
+      this.get('backgroundWidths') +
+      'overflow:visible;'+
+      'position:relative;'+
+      'height:10px;'+
+      'margin-top:10px;'+
+      'margin-left:5px;'+
+      'text-align:center;';
   }.property('overallWidth', 'backgroundWidths'),
-  currentValueStyles: function() {
+  currentValueStyle: function() {
     var right = ~~this.get('barRange1Percentage') + ~~this.get('barRange2Percentage') + ~~this.get('barRange3Percentage');
     right = 100 - right;
     if (right < 10) { // this is here for small case. consider extending later for different sizes.
@@ -44,87 +38,40 @@ App.ProgressBarStackedComponent = Ember.Component.extend({
     }
     return 'position:absolute; bottom:-14px; right:'+right+'%';
   }.property('value'),
+
   progressBarOnePercent: function() {
-    return (this.get('thresholdOne') - 0) / this.get('thresholdMax') * 100;
+    return 100 * this.get('thresholdOne') / this.get('thresholdMax');
   }.property('thresholdOne', 'thresholdMax', 'value'),
   progressBarTwoPercent: function() {
-    return (this.get('thresholdTwo') - this.get('thresholdOne')) / this.get('thresholdMax') * 100;
-  }.property('thresholdOne', 'thresholdTwo', 'thresholdMax', 'value'),  
+    return 100 * (this.get('thresholdTwo') - this.get('thresholdOne')) / this.get('thresholdMax');
+  }.property('thresholdOne', 'thresholdTwo', 'thresholdMax', 'value'),
   progressBarThreePercent: function() {
-    return (this.get('thresholdThree') - this.get('thresholdTwo')) / this.get('thresholdMax') * 100;
+    return 100 * (this.get('thresholdThree') - this.get('thresholdTwo')) / this.get('thresholdMax');
   }.property('thresholdTwo', 'thresholdThree', 'thresholdMax', 'value'),
+
   barRange1Percentage: function() {
-    var result = this.get('value') / this.get('thresholdOne');
-    var result = result * 100;
-    var result = result / 100;
-    var result = result * this.get('progressBarOnePercent');
-    if (result > this.get('progressBarOnePercent')) {
-      return this.get('progressBarOnePercent');
-    } else {
-      return result;
-    }
-  }.property('value', 'thresholdOne', 'progressBarOnePercent'),
-  barRange1PercentageWidth: function() {
+    var width = Math.min(this.get('value'), this.get('thresholdOne'));
+    width = Math.max(width, 0);
+    return 100 * width / this.get('thresholdMax');
+  }.property('value', 'thresholdOne', 'thresholdMax'),
+  barRange2Percentage: function() {
+    var width = Math.min(this.get('value'), this.get('thresholdTwo')) - this.get('thresholdOne');
+    width = Math.max(width, 0);
+    return 100 * width / this.get('thresholdMax');
+  }.property('value', 'thresholdOne', 'thresholdTwo'),
+  barRange3Percentage: function() {
+    var width = Math.min(this.get('value'), this.get('thresholdThree')) - this.get('thresholdTwo');
+    width = Math.max(width, 0);
+    return 100 * width / this.get('thresholdMax');
+  }.property('value', 'thresholdTwo','thresholdThree'),
+
+  barRange1Style: function() {
     return 'width:' + this.get('barRange1Percentage') + '%';
   }.property('barRange1Percentage'),
-  barRange2Percentage: function() {
-    if (this.get('value') > this.get('thresholdOne') && this.get('value') <= this.get('thresholdTwo')) {
-      if (this.get('areEvenThresholds')) {
-        var result = this.get('value') - this.get('thresholdOne');
-      } else {
-        var result = this.get('value');
-      }
-      if (result > 0) {
-        var result = result.toFixed(2);
-        if (result > 1) { 
-          var result = result / this.get('thresholdTwo'); 
-        }
-        var result = result * 100;
-        var result = result / 100;
-        var result = result * this.get('progressBarTwoPercent');
-        if (result > this.get('progressBarTwoPercent')) {
-          return this.get('progressBarTwoPercent');
-        } else {
-          return result;
-        }
-      }
-    } else if (this.get('value') >= this.get('thresholdTwo')) {
-      return this.get('progressBarTwoPercent');
-    } else {
-      return 0;
-    }
-  }.property('value', 'thresholdOne', 'thresholdTwo', 'progressBarTwoPercent'),
-  barRange2PercentageWidth: function() {
+  barRange2Style: function() {
     return 'width:' + this.get('barRange2Percentage') + '%';
   }.property('barRange2Percentage'),
-  barRange3Percentage: function() {
-    if (this.get('value') > this.get('thresholdTwo') && this.get('value') <= this.get('thresholdThree')) {
-      if (this.get('areEvenThresholds')) {
-        var result = this.get('value') - this.get('thresholdTwo');
-      } else {
-        var result = this.get('value');
-      }
-      if (result > 0) {
-        var result = result.toFixed(2);
-        if (result > 1) { 
-          var result = result / this.get('thresholdThree'); 
-        }
-        var result = result * 100;
-        var result = result / 100;
-        var result = result * this.get('progressBarThreePercent');
-        if (result > this.get('progressBarThreePercent')) {
-          return this.get('progressBarThreePercent');
-        } else {
-          return result;
-        }
-      }
-    } else if (this.get('value') >= this.get('thresholdThree')) {
-      return this.get('progressBarThreePercent');
-    } else {
-      return 0;
-    }
-  }.property('value', 'thresholdOne', 'thresholdTwo','thresholdThree', 'progressBarThreePercent'),
-  barRange3PercentageWidth: function() {
+  barRange3Style: function() {
     return 'width:' + this.get('barRange3Percentage') + '%';
   }.property('barRange3Percentage')
 });
