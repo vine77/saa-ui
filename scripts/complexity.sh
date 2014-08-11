ff5a97ac744bbc943ed4cfd5033a958f4292a3ff
Nathan Ward <ward@digivine.com>
2014-08-11 01:47:58 -0700

    Add script to output git report

---
+++ scripts/complexity.sh
@@ -0,0 +1,14 @@
+cd ~/saa-ui
+git checkout $(git rev-list -n 1 --before="$(node -p "require('moment')().startOf('month').subtract(5, 'month').format()")" master)
+plato -r -d ~/plato/saa-ui -t "SAA UI" -D `node -p "require('moment')().startOf('month').subtract(5, 'month').unix()"` ~/saa-ui/scripts/**/*.js
+git checkout $(git rev-list -n 1 --before="$(node -p "require('moment')().startOf('month').subtract(4, 'month').format()")" master)
+plato -r -d ~/plato/saa-ui -t "SAA UI" -D `node -p "require('moment')().startOf('month').subtract(4, 'month').unix()"` ~/saa-ui/scripts/**/*.js
+git checkout $(git rev-list -n 1 --before="$(node -p "require('moment')().startOf('month').subtract(3, 'month').format()")" master)
+plato -r -d ~/plato/saa-ui -t "SAA UI" -D `node -p "require('moment')().startOf('month').subtract(3, 'month').unix()"` ~/saa-ui/scripts/**/*.js
+git checkout $(git rev-list -n 1 --before="$(node -p "require('moment')().startOf('month').subtract(2, 'month').format()")" master)
+plato -r -d ~/plato/saa-ui -t "SAA UI" -D `node -p "require('moment')().startOf('month').subtract(2, 'month').unix()"` ~/saa-ui/scripts/**/*.js
+git checkout $(git rev-list -n 1 --before="$(node -p "require('moment')().startOf('month').subtract(1, 'month').format()")" master)
+plato -r -d ~/plato/saa-ui -t "SAA UI" -D `node -p "require('moment')().startOf('month').subtract(1, 'month').unix()"` ~/saa-ui/scripts/**/*.js
+git checkout $(git rev-list -n 1 --before="$(node -p "require('moment')().startOf('month').format()")" master)
+plato -r -d ~/plato/saa-ui -t "SAA UI" -D `node -p "require('moment')().startOf('month').unix()"` ~/saa-ui/scripts/**/*.js
+git checkout master
