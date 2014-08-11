4701d43d555c10ab046e873258ded4bfe1ee3dca
Nathan Ward <ward@digivine.com>
2014-08-11 00:31:15 -0700

    Generate monthly code quality reports for SAA

--- complexity.sh
+++ complexity.sh
@@ -1,13 +1,13 @@
 cd ~/saa-ui
-git checkout `git rev-list -n 1 --before="5 months ago" master`
+git checkout $(git rev-list -n 1 --before="$(node -p "require('moment')().startOf('month').subtract(5, 'month').format()")" master)
 plato -r -d ~/plato/saa-ui -t "SAA UI" -D `node -p "require('moment')().startOf('month').subtract(5, 'month').unix()"` ~/saa-ui/scripts/**/*.js
-git checkout `git rev-list -n 1 --before="4 months ago" master`
+git checkout $(git rev-list -n 1 --before="$(node -p "require('moment')().startOf('month').subtract(4, 'month').format()")" master)
 plato -r -d ~/plato/saa-ui -t "SAA UI" -D `node -p "require('moment')().startOf('month').subtract(4, 'month').unix()"` ~/saa-ui/scripts/**/*.js
-git checkout `git rev-list -n 1 --before="3 months ago" master`
+git checkout $(git rev-list -n 1 --before="$(node -p "require('moment')().startOf('month').subtract(3, 'month').format()")" master)
 plato -r -d ~/plato/saa-ui -t "SAA UI" -D `node -p "require('moment')().startOf('month').subtract(3, 'month').unix()"` ~/saa-ui/scripts/**/*.js
-git checkout `git rev-list -n 1 --before="2 months ago" master`
+git checkout $(git rev-list -n 1 --before="$(node -p "require('moment')().startOf('month').subtract(2, 'month').format()")" master)
 plato -r -d ~/plato/saa-ui -t "SAA UI" -D `node -p "require('moment')().startOf('month').subtract(2, 'month').unix()"` ~/saa-ui/scripts/**/*.js
-git checkout `git rev-list -n 1 --before="1 months ago" master`
+git checkout $(git rev-list -n 1 --before="$(node -p "require('moment')().startOf('month').subtract(1, 'month').format()")" master)
 plato -r -d ~/plato/saa-ui -t "SAA UI" -D `node -p "require('moment')().startOf('month').subtract(1, 'month').unix()"` ~/saa-ui/scripts/**/*.js
 git checkout master
 plato -r -d ~/plato/saa-ui -t "SAA UI" ~/saa-ui/scripts/{*.js,**/*.js}
