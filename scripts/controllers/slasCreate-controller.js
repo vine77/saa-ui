f5b167f2370f2f7c2ab7b148ff95ca590401bada
GregoryX Houchens <gregoryx.houchens@intel.com>
2014-08-21 10:26:15 -0700

    Add SCU dynamic tables on SLA editing and creation

--- scripts/controllers/slasCreate-controller.js
+++ scripts/controllers/slasCreate-controller.js
@@ -1,9 +1,157 @@
 App.SlasCreateController = Ember.ObjectController.extend({
   needs: ['nodes'],
+
+  bucketSloCountGreaterThanOne: function() {
+    return (this.get('bucketSloCount') >= 1);
+  }.property('bucketSloCount'),
+  sloTemplates: function() {
+    var self = this;
+    var returnArray = this.store.all('sloTemplate').map(function(item, index, enumerable) {
+      var disabled = false;
+      switch(item.get('sloType')) {
+        case 'compute':
+          if (self.get('bucketSloCount') >= 1) {
+            disabled = true;
+          }
+          break;
+        case 'vm_compute':
+          if (self.get('bucketSloCount') >= 1) {
+            disabled = true;
+          }
+          break;
+        case 'vm_cores':
+          if (self.get('bucketSloCount') >= 1) {
+            disabled = true;
+          }
+          break;
+        case 'trusted_platform':
+          if (self.get('trustSloCount') >= 1) {
+            disabled = true;
+          }
+          break;
+        default:
+          disabled = false;
+          break;
+      }
+      item.disabled = disabled;
+      return item;
+    });
+    return returnArray;
+  }.property('isAddSloAvailable', 'sloTypesArray.@each'),
+  bucketSloCount: function() {
+    var computeCount = this.get('sloTypesArray') && this.get('sloTypesArray').filter(function(x){ return x == 'compute'; }).get('length');
+    var vmComputeCount = this.get('sloTypesArray') && this.get('sloTypesArray').filter(function(x){ return x == 'vm_compute'; }).get('length');
+    var vmCoresCount = this.get('sloTypesArray') && this.get('sloTypesArray').filter(function(x){ return x == 'vm_cores'; }).get('length');
+    return computeCount + vmComputeCount + vmCoresCount;
+  }.property('sloTypesArray.@each', 'sloTypesArray'),
+  trustSloCount: function () {
+    return this.get('sloTypesArray') && this.get('sloTypesArray').filter(function(x){ return x == 'trusted_platform'; }).get('length');
+  }.property('sloTypesArray.@each', 'sloTypesArray'),
+  isAddSloAvailable: function() {
+    return !(this.get('bucketSloCount') >= 1 && this.get('trustSloCount') >= 1);
+  }.property('sloTypesArray.@each', 'sloTypesArray', 'bucketSloCount', 'trustSloCount'),
+
+  vcpuValues: [0, 1, 2, 3, 4],
+  vcpus: 0,
+
+  isComputeSloTable: function() {
+    return this.get('sloTypesArray') && this.get('sloTypesArray').contains('compute');
+  }.property('sloTypesArray.@each'),
+  isComputeVmSloTable: function() {
+    return this.get('sloTypesArray') && this.get('sloTypesArray').contains('vm_compute');
+  }.property('sloTypesArray.@each'),
+  isExclusiveCoresSloTable: function() {
+    return this.get('sloTypesArray') && this.get('sloTypesArray').contains('vm_cores');
+  }.property('sloTypesArray.@each'),
+  isSloTableVisible: function() {
+    return (!!this.get('isComputeSloTable') || !!this.get('isComputeVmSloTable') || !!this.get('isExclusiveCoresSloTable'));
+  }.property('isComputeSloTable', 'isComputeVmSloTable', 'isExclusiveCoresSloTable'),
+
+  sloVcpuTableMaximumScus: function() {
+    return this.get('controllers.nodes.maxScuCapabilities');
+  }.property('controllers.nodes.maxScuCapabilities', 'vcpus', 'isComputeVmSloTable'),
+  sloVcpuTableNumberOfNodesMaximumScus: function() {
+    var self = this;
+    return this.get('controllers.nodes.model').filterBy('isAssured').filterBy('capabilities.max_scu_per_core').reduce( function(previousValue, item, index, enumerable) {
+      return previousValue + (item.get('capabilities.max_scu_per_core') >= self.get('controllers.nodes.maxScuCapabilities'));
+    }, 0);
+  }.property('controllers.nodes.model.@each', 'controllers.nodes'),
+  sloVcpuTableMedianScus: function() {
+    return this.get('controllers.nodes.medianScuCapabilities');
+  }.property('controllers.nodes.medianScuCapabilities', 'vcpus'),
+  sloVcpuTableNumberOfNodesMedianScus: function() {
+    var self = this;
+    return this.get('controllers.nodes.model').filterBy('isAssured').filterBy('capabilities.max_scu_per_core').reduce( function(previousValue, item, index, enumerable) {
+      return previousValue + (item.get('capabilities.max_scu_per_core') >= self.get('controllers.nodes.maxScuCapabilities'));
+    }, 0);
+  }.property('controllers.nodes.model.@each'),
+  sloVcpuTableMinimumScus: function() {
+    return this.get('controllers.nodes.minScuCapabilities');
+  }.property('controllers.nodes.minScuCapabilities', 'vcpus'),
+  sloVcpuTableNumberOfNodesMinimumScus: function() {
+    var self = this;
+    return this.get('controllers.nodes.model').filterBy('isAssured').filterBy('capabilities.max_scu_per_core').reduce( function(previousValue, item, index, enumerable) {
+      return previousValue + (item.get('capabilities.max_scu_per_core') >= self.get('controllers.nodes.minScuCapabilities'));
+    }, 0);
+  }.property('controllers.nodes.model.@each'),
+
+  sloVmTableMaximumScus: function() {
+    return this.get('controllers.nodes.maxScuCapabilities') * this.get('vcpus');
+  }.property('controllers.nodes.maxScuCapabilities', 'vcpus', 'isComputeVmSloTable'),
+  sloVmTableNumberOfNodesMaximumScus: function() {
+    var self = this;
+    return this.get('controllers.nodes.model').filterBy('isAssured').filterBy('capabilities.max_scu_per_core').reduce( function(previousValue, item, index, enumerable) {
+      return previousValue + (item.get('capabilities.max_scu_per_core') >= self.get('controllers.nodes.maxScuCapabilities'));
+    }, 0);
+  }.property('controllers.nodes.model.@each', 'controllers.nodes'),
+  sloVmTableMedianScus: function() {
+    return this.get('controllers.nodes.medianScuCapabilities') * this.get('vcpus');
+  }.property('controllers.nodes.medianScuCapabilities', 'vcpus'),
+  sloVmTableNumberOfNodesMedianScus: function() {
+    var self = this;
+    return this.get('controllers.nodes.model').filterBy('isAssured').filterBy('capabilities.max_scu_per_core').reduce( function(previousValue, item, index, enumerable) {
+      return previousValue + (item.get('capabilities.max_scu_per_core') >= self.get('controllers.nodes.maxScuCapabilities'));
+    }, 0);
+  }.property('controllers.nodes.model.@each'),
+  sloVmTableMinimumScus: function() {
+    return this.get('controllers.nodes.minScuCapabilities') * this.get('vcpus');
+  }.property('controllers.nodes.minScuCapabilities', 'vcpus'),
+  sloVmTableNumberOfNodesMinimumScus: function() {
+    var self = this;
+    return this.get('controllers.nodes.model').filterBy('isAssured').filterBy('capabilities.max_scu_per_core').reduce( function(previousValue, item, index, enumerable) {
+      return previousValue + (item.get('capabilities.max_scu_per_core') >= self.get('controllers.nodes.minScuCapabilities'));
+    }, 0);
+  }.property('controllers.nodes.model.@each'),
+
+  sloExclusiveCoresTableMaximum: function() {
+    return this.get('controllers.nodes.maxScuCapabilities');
+  }.property('controllers.nodes.maxScuCapabilities', 'vcpus', 'isComputeVmSloTable'),
+  sloExclusiveCoresTableNumberOfNodesMaximum: function() {
+    var self = this;
+    return this.get('controllers.nodes.model').filterBy('isAssured').filterBy('capabilities.max_scu_per_core').reduce( function(previousValue, item, index, enumerable) {
+      return previousValue + (item.get('capabilities.cores_per_socket') >= self.get('controllers.nodes.maxScuCapabilities'));
+    }, 0);
+  }.property('controllers.nodes.model.@each', 'controllers.nodes'),
+  sloExclusiveCoresTableMedian: function() {
+    return this.get('controllers.nodes.medianScuCapabilities');
+  }.property('controllers.nodes.medianScuCapabilities', 'vcpus'),
+  sloExclusiveCoresTableNumberOfNodesMedian: function() {
+    var self = this;
+    return this.get('controllers.nodes.model').filterBy('isAssured').filterBy('capabilities.max_scu_per_core').reduce( function(previousValue, item, index, enumerable) {
+      return previousValue + (item.get('capabilities.cores_per_socket') >= self.get('controllers.nodes.maxScuCapabilities'));
+    }, 0);
+  }.property('controllers.nodes.model.@each'),
+  sloExclusiveCoresTableMinimum: function() {
+    return this.get('controllers.nodes.minScuCapabilities');
+  }.property('controllers.nodes.minScuCapabilities', 'vcpus'),
+  sloExclusiveCoresTableNumberOfNodesMinimum: function() {
+    var self = this;
+    return this.get('controllers.nodes.model').filterBy('isAssured').filterBy('capabilities.max_scu_per_core').reduce( function(previousValue, item, index, enumerable) {
+      return previousValue + (item.get('capabilities.cores_per_socket') >= self.get('controllers.nodes.minScuCapabilities'));
+    }, 0);
+  }.property('controllers.nodes.model.@each'),
+
   isSlaCreating: false,
-  sloTemplates: function () {
-    return this.store.all('sloTemplate');
-  }.property(),
   actions: {
     addSlo: function () {
       this.get('model.slos').addObject(this.store.createRecord('slo', {id: App.uuid()}));
