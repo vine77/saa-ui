e2b9d67f8ef16cc10b8be3af4e3d6a1c9402b156
GregoryX Houchens <gregoryx.houchens@intel.com>
2014-08-22 18:24:58 -0700

    Add new container for SLO table for increasing separation and visibility

--- scripts/controllers/flavorEdit-controller.js
+++ scripts/controllers/flavorEdit-controller.js
@@ -1,6 +1,9 @@
 App.FlavorEditController = Ember.ObjectController.extend({
   needs: ['flavors', 'slas', 'nodes'],
 
+  bucketSloCountGreaterThanOne: function() {
+    return (this.get('bucketSloCount') >= 1);
+  }.property('bucketSloCount'),
   sloTemplates: function() {
     var self = this;
     var returnArray = this.store.all('sloTemplate').map(function(item, index, enumerable) {
