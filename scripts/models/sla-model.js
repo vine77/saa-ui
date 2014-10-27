c3ff6eef32b47ad4719621c28f7c1406938a8059
Nathan Ward <ward@digivine.com>
2014-10-27 11:43:58 -0400

    Compose trust SLO value in SLAs table instead of just SLO type
    Closes #107, closes #EPSD100244869

--- scripts/models/sla-model.js
+++ scripts/models/sla-model.js
@@ -25,6 +25,9 @@ App.Sla = DS.Model.extend({
     //return this.get('slos').getEach('sloType').toString();
     return this.get('slos').map(function (item, index, enumerable) {
       if (!item || !item.get('sloType')) return null;
+      if (item.get('sloType') === 'trusted_platform') {
+        return (!item.get('value') || item.get('value') === '0') ? 'Untrusted' : 'Trusted';
+      }
       return item.get('sloType').replace(/[_-]/g, ' ').capitalize();
     }).join(', ');
   }.property('slos.@each.sloType'),
