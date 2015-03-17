a64d69c998214370d2c841e03a625df91c806caf
GregoryX Houchens <gregoryx.houchens@intel.com>
2015-03-17 07:32:17 -0700

    Add IP header for sunburst component

--- scripts/controllers/sunburstComponent-controller.js
+++ scripts/controllers/sunburstComponent-controller.js
@@ -1,4 +1,16 @@
 /**
+ * Portions of this file are from Sequences Sunburst (https://gist.github.com/kerryrodden/7090426), Copyright 2013 Google Inc
+ *
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+/**
  * Parameters for usage:
  * @parameter: dataSource
  * Needed to be in the tree/sunburst layout as specified in d3. Size is specified on each node.
@@ -7,7 +19,9 @@
  * @parameter: height
  * @parameter: units
  * @parameter: legend
+ *
  */
+
 App.SunburstChartComponent = Ember.Component.extend({
   width: 750,
   height: 600,
