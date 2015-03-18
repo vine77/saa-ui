5306961f0a1d101d3ddd0efe58d6c26f3429e55a
Nathan Ward <ward@digivine.com>
2015-03-18 16:41:32 -0700

    Implement disabled/pending save button on overrides settings screen
    Addresses #EPSD100245486

--- scripts/controllers/settingsNetwork-controller.js
+++ scripts/controllers/settingsNetwork-controller.js
@@ -15,38 +15,38 @@ App.SettingsNetworkController = Ember.Controller.extend({
     }
   }.property('networkType.setting'),
   actions: {
-    save: function () {
-      var self = this;
-      this.store.find('override', 'current').then(function(current) {
-        current.set('configurationValues', self.get('configurationValues'));
-        current.save().then( function() {
-           App.event('Successfully updated configuration overrides.', App.SUCCESS);
-        }, function(xhr) {
-          App.xhrError(xhr, 'An error occurred while attempting to override configuration values.');
-        });
+    updateNetworkOverrides: function() {
+      var currentOverrides = this.store.getById('override', 'current');
+      currentOverrides.set('configurationValues', this.get('configurationValues'));
+      return currentOverrides.save().then(function() {
+        App.event('Successfully updated configuration override values.', App.SUCCESS);
+      }, function(xhr) {
+        App.xhrError(xhr, 'An error occurred while attempting to save configuration override values.');
       });
-
-      var externalIpChanged = App.network.get('external.address') != App.network.get('serverExternal.address');
+    },
+    saveNetworkSettings: function() {
+      var self = this;
+      this.set('isActionPending', true);
+      App.network.save().then(function() {
+        self.set('isActionPending', false);
+        self.send('updateNetworkOverrides');
+      }, function() {
+        self.set('isActionPending', false);
+      });
+    },
+    save: function() {
+      var self = this;
+      var externalIpChanged = App.network.get('external.address') !== App.network.get('serverExternal.address');
       var externalChangedToDynamic = (App.network.get('external.dhcp') && App.network.get('external.dhcp') != App.network.get('serverExternal.dhcp'));
       if (externalChangedToDynamic) {
         var verify = confirm('You have changed your external interface to use DHCP, so your IP address may change, disconnecting this application, and you will have to navigate to the new location. Are you sure you want to proceed?');
         if (verify) {
-          this.set('isActionPending', true);
-          App.network.save().then(function () {
-            self.set('isActionPending', false);
-          }, function () {
-            self.set('isActionPending', false);
-          });
+          this.send('saveNetworkSettings');
         }
       } else if (externalIpChanged) {
         var verify = confirm('You have changed your external interface\'s IP address, so this application will become disconnected and you will have to navigate to the new location. Are you sure you want to proceed?');
         if (verify) {
-          this.set('isActionPending', true);
-          App.network.save().then(function () {
-            self.set('isActionPending', false);
-          }, function () {
-            self.set('isActionPending', false);
-          });
+          this.send('saveNetworkSettings');
           /* The following could be done if the netconfig API response didn't timeout
           App.network.save().then(function (json) {
             window.location =  window.location.protocol + '//' + json.external.address + '/#/settings/network';
@@ -54,17 +54,12 @@ App.SettingsNetworkController = Ember.Controller.extend({
           */
         }
       } else {
-        this.set('isActionPending', true);
-        App.network.save().then(function () {
-          self.set('isActionPending', false);
-        }, function () {
-          self.set('isActionPending', false);
-        });
+        this.send('saveNetworkSettings');
       }
     },
     cancel: function () {
-      App.network.check();
       var self = this;
+      App.network.check();
       this.store.find('override', 'current').then(function(current) {
         current.reload();
         self.set('configurationValues', current.get('configurationValues'));
--- scripts/controllers/settingsOverrides-controller.js
+++ scripts/controllers/settingsOverrides-controller.js
@@ -1,11 +1,16 @@
 App.SettingsOverridesController = Ember.ObjectController.extend({
   configurationValuesExist: Ember.computed.gt('model.configurationValues.length', 0),
+  isActionPending: false,
   actions: {
     updateOverrides: function() {
+      var self = this;
+      this.set('isActionPending', true);
       return this.store.getById('override', 'current').save().then(function() {
-        App.event('Successfully updated configuration overrides.', App.SUCCESS);
+        self.set('isActionPending', false);
+        App.event('Successfully updated configuration override values.', App.SUCCESS);
       }, function(xhr) {
-        App.xhrError(xhr, 'An error occurred while attempting to override configuration values.');
+        self.set('isActionPending', false);
+        App.xhrError(xhr, 'An error occurred while attempting to save configuration override values.');
       });
     },
     cancel: function() {
