0cc22299f6e9682dacb68e76207524c0ea76bf59
GregoryX Houchens <gregoryx.houchens@intel.com>
2015-03-31 12:46:25 -0700

    Extend tooltip view to support width data attribute binding
    Concerns #EPSD100245635

--- scripts/views/tooltip-view.js
+++ scripts/views/tooltip-view.js
@@ -1,8 +1,9 @@
 App.TooltipView = Ember.View.extend({
   classNames: ['inline-block', 'tooltip-container'],
-  attributeBindings: ['title', 'dataHtml:data-html','dataSelector:data-selector', 'dataContainer:data-container', 'dataPlacement:data-placement', 'displayFlag'],
+  attributeBindings: ['title','style', 'dataHtml:data-html','dataSelector:data-selector', 'dataContainer:data-container', 'dataPlacement:data-placement', 'displayFlag'],
   dataHtml: false,
   placement: 'top',
+  style: 'width:100%',
   mouseEnter: function (event) {
     $('#' + this.get('elementId')).tooltip({container: 'body', placement: this.get('placement')}).tooltip('fixTitle').tooltip('show');
   },
