1ba550099c9d1512c8620ba6716799a5f46a9e8e
Nathan Ward <ward@digivine.com>
2015-04-06 03:45:28 -0700

    Adjust tooltip hover target widths
    Addresses #EPSD100245635

--- scripts/views/tooltip-view.js
+++ scripts/views/tooltip-view.js
@@ -1,9 +1,8 @@
 App.TooltipView = Ember.View.extend({
   classNames: ['inline-block', 'tooltip-container'],
-  attributeBindings: ['title','style', 'dataHtml:data-html','dataSelector:data-selector', 'dataContainer:data-container', 'dataPlacement:data-placement', 'displayFlag'],
+  attributeBindings: ['title', 'style', 'dataHtml:data-html','dataSelector:data-selector', 'dataContainer:data-container', 'dataPlacement:data-placement', 'displayFlag'],
   dataHtml: false,
   placement: 'top',
-  style: 'width:100%',
   mouseEnter: function (event) {
     $('#' + this.get('elementId')).tooltip({container: 'body', placement: this.get('placement')}).tooltip('fixTitle').tooltip('show');
   },
